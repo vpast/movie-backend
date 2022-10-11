e56e704b70f9ee307004aa74d0307a3baebbe059
vpast <vpast.dev@gmail.com>
2022-10-11 18:56:40 +0300

    connection

--- model/mongo/db.js
+++ model/mongo/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const CONNECTION =
-  process.env.CONNECTION || 'mongodb://127.0.0.1:27017/full_stack_3';
+  process.env.CONNECTION || 'mongodb://moviesdb:2b0e4222c799a6326f749c9a7984ce6f@dokku-mongo-moviesdb:27017/moviesdb';
 
 mongoose.connect(CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// mongodb://127.0.0.1:27017/movie_catalog_backend
