e32601ecb98a5c41b2784a31d39773f337e496a4
vpast <vpast.dev@gmail.com>
2024-08-30 16:34:33 +0200

    connect to Atlas

--- model/mongo/db.js
+++ model/mongo/db.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
-const CONNECTION = process.env.CONNECTION || 'mongodb://localhost:27017/movies';
+// const CONNECTION = 'mongodb+srv://vpastdev:Ph0mSNgoEFYnpRjJ@movies.4xtpp.mongodb.net/';
+const CONNECTION = 'mongodb+srv://vpastdev:Ph0mSNgoEFYnpRjJ@movies.4xtpp.mongodb.net/movies?retryWrites=true&w=majority';
 
 
 mongoose.set('debug', true);
 
 mongoose.connect(CONNECTION, {});
+
+// 'mongodb://localhost:27017/movies'
+
+// process.env.CONNECTION ||
 
 // mongodb://127.0.0.1:27017/movie_catalog_backend
